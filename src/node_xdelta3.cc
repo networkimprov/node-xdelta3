cbe404ce0e4fd7f207a19dc72a61a36ec6b854ca
mtibeica <mtibeica@gmail.com>
2013-03-30 00:59:22 -0700

    add: node_xdelta3.cc, xdelta3.js - DiffChunked async call and emit handled properly in xdelta3#diff

--- src/node_xdelta3.cc
+++ src/node_xdelta3.cc
@@ -5,12 +5,54 @@
 
 using namespace v8;
 
+struct DiffChunked_data {
+  DiffChunked_data(int s, int d, Handle<Function> cbData, Handle<Function> cbEnd) : src(s), dst(d), callbackData(cbData), callbackEnd(cbEnd), done(false) {}
+  int src, dst;
+  Persistent<Function> callbackData, callbackEnd;
+  bool done;
+};
+
+void DiffChunked_pool(uv_work_t* req) {
+  DiffChunked_data* aData = (DiffChunked_data*) req->data;
+  aData->done = true;
+}
+
+void DiffChunked_done(uv_work_t* req, int ) {
+  HandleScope scope;
+  DiffChunked_data* aData = (DiffChunked_data*) req->data;
+  
+  if (aData->done) {
+    //emit the end
+    TryCatch try_catch;
+    aData->callbackEnd->Call(Context::GetCurrent()->Global(), 0, NULL);
+    if (try_catch.HasCaught())
+      node::FatalException(try_catch);
+    aData->callbackData.Dispose();
+    aData->callbackEnd.Dispose();
+    delete req;  
+    delete aData;
+  } else {
+    //emit the buffer chunk
+    //keep on working
+    uv_queue_work(uv_default_loop(), req, DiffChunked_pool, DiffChunked_done);
+  }
+
+}
+
 Handle<Value> DiffChunked(const Arguments& args) {
   HandleScope scope;
 
-  xd3_config config;
-  xd3_init_config(&config, XD3_ADLER32);
+  if (args.Length() < 4 || !args[0]->IsInt32() || !args[1]->IsInt32() || !args[2]->IsFunction() || !args[3]->IsFunction()) {
+    return ThrowException(Exception::TypeError(String::New("arguments are (fd, fd, function, function)")));
+  }
 
+  DiffChunked_data* aData;
+  aData = new DiffChunked_data(args[0]->Uint32Value(), args[1]->Uint32Value(), Persistent<Function>::New(Local<Function>::Cast(args[2])), Persistent<Function>::New(Local<Function>::Cast(args[3])));
+  uv_work_t* aReq = new uv_work_t();
+  aReq->data = aData;
+
+  uv_queue_work(uv_default_loop(), aReq, DiffChunked_pool, DiffChunked_done);
+  
   return scope.Close(String::New("not developed"));
 }
 
@@ -19,3 +61,4 @@ void init(Handle<Object> exports) {
 }
 
 NODE_MODULE(node_xdelta3, init)
+
--- xdelta3.js
+++ xdelta3.js
@@ -4,15 +4,14 @@ var xdelta = require('./build/Release/node_xdelta3.node');
 function diff(src, dst) {
   var aStream = new Stream;
   aStream.readable = true;
-
-  //TODO: call C++ method which will emit chunks as diff gets to a certain size
-  xdelta.diff_chunked();
-
-  setTimeout(function (){
-    aStream.emit('data', new Buffer('not yet developed'));
-    aStream.emit('end');
-  }, 1000);
-
+  xdelta.diff_chunked( src, dst,
+    function (buf) {
+      aStream.emit('data', buf);
+    },
+    function () {
+      aStream.emit('end');
+    }
+  );
   return aStream;
 }
 
