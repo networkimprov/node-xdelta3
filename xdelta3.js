608fc15ce58d43e1d51651194133b85c3fa77334
mtibeica <mtibeica@gmail.com>
2013-04-07 01:46:29 -0700

    add: xdelta3.js - xdelta3#diff and xdelta3#patch using node 0.10 stream interface

--- xdelta3.js
+++ xdelta3.js
@@ -2,15 +2,15 @@ var Stream = require('stream');
 var xdelta = require('./build/Release/node_xdelta3.node');
 
 function diff(src, dst) {
-  var aStream = new Stream;
-  aStream.readable = true;
+  var aStream = new Stream.Readable();
+  aStream._read = function(size) { /*TODO: pause and resume diff_chunked*/};
   xdelta.diff_chunked( src, dst,
     function (buf) {
-      aStream.emit('data', buf);
+      aStream.push(buf);
     },
     function (err) {
       if (typeof(err) == 'undefined')
-        aStream.emit('end');
+        aStream.push(null);
       else
         aStream.emit('error', err);
     }
@@ -19,17 +19,11 @@ function diff(src, dst) {
 }
 
 function patch(src, dst, callback) {
-  var aWritableStream = new Stream;
-  aWritableStream.writable = true;
+  var aWritableStream = new Stream.Writable();
 
-  aWritableStream.write = function (buf) {
+  aWritableStream._write = function (chunk, encoding, callback) {
     //TODO: call C++ method which processes a diff chunk
     console.log('patch buf: ' + buf.toString());
-  }
-
-  aWritableStream.end = function() {
-    //TODO: call C++ method which signals the end of a patch
-    callback();
   }
 
   return aWritableStream;
